b64a38e162eff684b4f176068b841dcfe4135446
Kristian Koehntopp <kris-git@koehntopp.de>
2017-08-07 00:49:53 +0200

    sha-create.py: change progress indicator
    To illustrate how the database becomes slower as the row count
    goes up, we change the output format for the progress indicator.
    We now print an ongoing timing and rows/sec update.

--- mysql/sha-create.py
+++ mysql/sha-create.py
@@ -31,7 +31,8 @@ def main():
         """, (hashlib.sha1(b'%d' % (i,)).hexdigest(),))
 
         if i % 100000 == 0:
-            print('%d...' % (i,), end='', flush=True)
+            now = time.time()
+            print('%d rows, %.2f seconds, %.2f inserts/s' % (i, now-start, COUNT / (now-start)), flush=True)
             conn.commit()
 
     conn.commit()
