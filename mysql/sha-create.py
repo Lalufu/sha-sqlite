c10544a099049e00525269ed0d9d29613d12ad18
Kristian Koehntopp <kris-git@koehntopp.de>
2017-08-06 23:35:56 +0200

    sha-create.py: add sample run results

--- mysql/sha-create.py
+++ mysql/sha-create.py
@@ -6,6 +6,8 @@ current directory.
 The table must exist (use "mysql lalufu < sha.sql" to create
 it.
 
+=====
+
 Test Run: Intel(R) Core(TM) i7-3770 CPU @ 3.40GHz (8 Threads), 
           32GB RAM, 
           Crucial M4-CT512M4SSD2
@@ -14,11 +16,25 @@ Sample run with
 mysql> SET GLOBAL key_buffer_size = 16*1024*1024*1024;
 
 $ ./sha-create.py
-0...100000
+0...100000 [...]35900000...
+2273.43 seconds, 15835.10 inserts/s
+Created index in 131.92 seconds
 
+mysql> select table_name
+            , engine
+            , row_format
+            , table_rows
+            , data_length/1024/1024 as data_mb
+            , index_length/1024/1024 as index_mb
+         from information_schema.tables
+where table_name ='sha';
++------------+--------+------------+------------+---------------+---------------+
+| table_name | engine | row_format | table_rows | data_mb       | index_mb      |
++------------+--------+------------+------------+---------------+---------------+
+| sha        | MyISAM | Dynamic    |   36000000 | 1785.27832031 | 1894.82910156 |
++------------+--------+------------+------------+---------------+---------------+
+1 row in set (0.00 sec)
 
-Sample run with
-mysql> SET GLOBAL innodb_buffer_pool_size = 16*1024*1024*1024;
 
 """
 
